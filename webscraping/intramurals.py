ee98fd3e270d908067cca2c2e191edd8307f62f2
Vince <phucvn166@gmail.com>
2024-10-13 19:26:01 -0400

    Updated intramurals.py

--- webscraping/intramurals.py
+++ webscraping/intramurals.py
@@ -3,7 +3,7 @@ import requests
 page_to_scrape = requests.get("https://athletics.uwaterloo.ca/sports/2010/7/21/Instructional_Classes.aspx")
 soup = BeautifulSoup(page_to_scrape.text, "html.parser")
 program_names = soup.findAll("span", attrs={"class":"flex-item-1"})
-class_names = soup.find("h4", class_=False)
+class_names = soup.find_all("h4", class_= '')
 
 for programs in program_names:
     print(programs.text)
