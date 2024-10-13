51d32535b628904ee561491557cb62ba6bdab31c
Vince <phucvn166@gmail.com>
2024-10-13 19:47:33 -0400

    Updated intramurals.py

--- webscraping/intramurals.py
+++ webscraping/intramurals.py
@@ -10,11 +10,11 @@ class_names = soup.find_all("h4", class_= '')
 file = open("intramurals.csv", "w")
 writer = csv.writer(file)
 
-writer.writerow("Programs", "Classes")
+writer.writerow(["Programs", "Classes"])
 
 for programs, classes in zip(program_names, class_names):
     print(programs.text + " " + classes.text)
     writer.writerow([programs.text, classes.text])
-    
+
 file.close()
 
