070421f3ec5b08108cc8d91fcc8d807fa2f356f5
Vince <phucvn166@gmail.com>
2024-10-13 19:32:59 -0400

    Updated intramurals.py

--- webscraping/intramurals.py
+++ webscraping/intramurals.py
@@ -1,10 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
+import csv 
 
 page_to_scrape = requests.get("https://athletics.uwaterloo.ca/sports/2010/7/21/Instructional_Classes.aspx")
 soup = BeautifulSoup(page_to_scrape.text, "html.parser")
 program_names = soup.findAll("span", attrs={"class":"flex-item-1"})
 class_names = soup.find_all("h4", class_= '')
 
+file = open("intramurals.csv", "w")
+writer = csv.writer(file)
+
+writer.writerow("Programs", "Classes")
+
 for programs, classes in zip(program_names, class_names):
     print(programs.text + " " + classes.text)
+    writer.writerow([programs.text, classes.text])
+    
+file.close()
+
