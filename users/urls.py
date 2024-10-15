e4fc2a3611eddbfccbccfd2a97c98306fd4d87e2
Jonathan Gong <jonathangong2005@gmail.com>
2024-10-15 19:46:45 -0400

    added sample signup form

--- users/urls.py
+++ users/urls.py
@@ -4,3 +4,4 @@ from . import views
 urlpatterns = [
     path('confirmationEmail/', views.send_email)
 ]
+
--- users/views.py
+++ users/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from users.models import User
 from .scripts.confirmationEmail.sendEmail import sendEmail
 
 # Create your views here.
 
 def send_email(request):
-    sendEmail("bruce.wang15@outlook.com", "hi")
+    sendEmail("jonathangong2005@gmail.com", "hi")
     return HttpResponse('email sent')
+
+def index(request):
+    if request.method == "POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        password_name = request.POST.get('password_name')
+        verification_code = request.POST.get('verification_code')
+
+        s = User(first_name = first_name, last_name = last_name, 
+                email = email, password_name = password_name, verification_code = verification_code)
+        s.save()
+    return render(request, '/templates/2_signup_page.html')
+
