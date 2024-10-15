a89f4cd41495521ae58e8165a98944bb32a44422
Alex Qin <pdsalex2018@gmail.com>
2024-10-14 22:37:06 -0400

    added send email url

--- users/scripts/confirmationEmail/sendEmail.py
+++ users/scripts/confirmationEmail/sendEmail.py
@@ -2,30 +2,32 @@ import smtplib
 import ssl
 
 from email.message import EmailMessage
-from passwords import qanyi_password
+from .passwords import qanyi_password
 
 
 # app name: Python
 
-eSender = "qanyi27@gmail.com"
-ePassword = qanyi_password
 
-eReceiver = "pdsalex2018@gmail.com"
+def sendEmail(reciever, link):
+    eSender = "qanyi27@gmail.com"
+    ePassword = qanyi_password
 
-subject = "test"
-body = "test"
+    eReceiver = reciever 
 
-em = EmailMessage()
+    subject = "test"
+    body = "test"
 
-em['From'] = eSender
-em['To'] = eReceiver
-em['Subject'] = subject
-em.set_content(body)
+    em = EmailMessage()
 
-context = ssl.create_default_context()
+    em['From'] = eSender
+    em['To'] = eReceiver
+    em['Subject'] = subject
+    em.set_content(body)
 
-with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-    smtp.login(eSender, ePassword)
-    smtp.sendmail(eSender, eReceiver, em.as_string())
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login(eSender, ePassword)
+        smtp.sendmail(eSender, eReceiver, em.as_string())
 
 
--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .scripts.confirmationEmail.sendEmail import sendEmail
 
 # Create your views here.
+
+def send_email(request):
+    sendEmail("bruce.wang15@outlook.com", "hi")
+    return HttpResponse('email sent')
