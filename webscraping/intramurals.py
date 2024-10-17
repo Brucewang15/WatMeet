1fe20b48a2183a2f806ea33887369784bb1179e9
Vince <phucvn166@gmail.com>
2024-10-17 14:25:04 -0400

    Update intramurals.py

--- webscraping/intramurals.py
+++ webscraping/intramurals.py
@@ -1,5 +1,6 @@
 from selenium import webdriver #make sure to pip install selenium to use this
 from selenium.webdriver.chrome.service import Service
+# make sure python version and selenium matches up (compare pip -V and Python Interpreter)
 from selenium.webdriver.common.by import By
 import time
 import json
@@ -8,7 +9,6 @@ import json
 # Selenium Manager will automatically download it
 service = Service()
 options = webdriver.ChromeOptions()
-driver = webdriver.Chrome(service=service, options=options)
 
 # Set up the Selenium WebDriver (use the path where you installed ChromeDriver)
 driver = webdriver.Chrome(executable_path="C:\Users\PC\Documents\GitHub\WatMeet\google chromedriver\chromedriver-win64\chromedriver-win64\chromedriver.exe")
