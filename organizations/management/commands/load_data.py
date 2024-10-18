1942d0060b40af39c99263fc567a2cea8472105e
Alex Qin <pdsalex2018@gmail.com>
2024-10-18 14:08:08 -0400

    updated database with all clubs

---
+++ organizations/management/commands/load_data.py
@@ -0,0 +1,38 @@
+import json
+from django.core.management.base import BaseCommand
+from organizations.models import Organization
+from users.models import User
+
+
+# python manage.py load_data
+
+class Command(BaseCommand):
+    help = 'Load data from a JSON file'
+
+    def handle(self, *args, **kwargs):
+        # Load data from the JSON file
+        with open('./organizations/club_info.json', 'r') as file:
+            data = json.load(file)
+
+        random_user = User.objects.create(
+            first_name = "king",
+            last_name = "bruh",
+            username= "korok",
+            password = "12345678",
+            email = "qanyi27@gmail.com",
+            verification_code = "abcdefg"
+        )
+
+        for item in data:
+            # Create model instances
+            print(item["names"])
+            Organization.objects.create(
+                org_name = item["names"],
+                overview = item['overviews'],
+                user = random_user,
+                star_rating = 3.5,
+                ranking_num = 3,
+                org_type = 'club',
+                constitution = "bruh",
+            )
+        self.stdout.write(self.style.SUCCESS('Data loaded successfully'))
