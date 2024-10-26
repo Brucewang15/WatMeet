80626ff30d7ad813b6c098d651fb93abd1a92dd8
Bruce Wang <bruce.wang15@outlook.com>
2024-10-26 19:13:42 -0400

    worked on frontend for comments

--- static/src/components/MainPage/ClubsDisplay/IndividualClubPage.jsx
+++ static/src/components/MainPage/ClubsDisplay/IndividualClubPage.jsx
@@ -65,54 +65,63 @@ const IndividualClubPage = () => {
 
             <div className="commentCard">
                 <span className="commentTitle">Comments</span>
-                {[...allComments].reverse().map((comment) => (
-                    <div className="allCommentsContainer">                        
-                            <div className="comments">
-                                <div className="comment-react">
-                                    <button>
-                                        <svg fill="none" viewBox="0 0 24 24" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
+                {[...allComments].reverse().map((comment, index) => (
+                    <div className="allCommentsContainer">
+                        <div className="comments">
+                            <div class="like-wrapper">
+                                <input class="check" type="checkbox" id={`like-toggle-${index}`} />
+                                <label class="container" for={`like-toggle-${index}`}>
+                                    <svg
+                                        viewBox="0 0 512 512"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="icon inactive"
+                                    >
+                                        <path
+                                            d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8v-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5v3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20c0 0-.1-.1-.1-.1c0 0 0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5v3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2v-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z"
+                                        ></path>
+                                    </svg>
+                                    <svg
+                                        viewBox="0 0 512 512"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="icon active"
+                                    >
+                                        <path
+                                            d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"
+                                        ></path>
+                                    </svg>
+                                </label>
+                                <span className="like-text">{comment.comment_upvote}</span>
+                            </div>
+
+                            <div className="comment-container">
+                                <div className="user">
+                                    <div className="user-pic">
+                                        <svg fill="none" viewBox="0 0 24 24" height="20" width="20" xmlns="http://www.w3.org/2000/svg">
                                             <path
+                                                strokeLinejoin="round"
                                                 fill="#707277"
                                                 strokeLinecap="round"
                                                 strokeWidth="2"
                                                 stroke="#707277"
-                                                d="M19.4626 3.99415C16.7809 2.34923 14.4404 3.01211 13.0344 4.06801C12.4578 4.50096 12.1696 4.71743 12 4.71743C11.8304 4.71743 11.5422 4.50096 10.9656 4.06801C9.55962 3.01211 7.21909 2.34923 4.53744 3.99415C1.01807 6.15294 0.221721 13.2749 8.33953 19.2834C9.88572 20.4278 10.6588 21 12 21C13.3412 21 14.1143 20.4278 15.6605 19.2834C23.7783 13.2749 22.9819 6.15294 19.4626 3.99415Z"
+                                                d="M6.57757 15.4816C5.1628 16.324 1.45336 18.0441 3.71266 20.1966C4.81631 21.248 6.04549 22 7.59087 22H16.4091C17.9545 22 19.1837 21.248 20.2873 20.1966C22.5466 18.0441 18.8372 16.324 17.4224 15.4816C14.1048 13.5061 9.89519 13.5061 6.57757 15.4816Z"
+                                            ></path>
+                                            <path
+                                                strokeWidth="2"
+                                                fill="#707277"
+                                                stroke="#707277"
+                                                d="M16.5 6.5C16.5 8.98528 14.4853 11 12 11C9.51472 11 7.5 8.98528 7.5 6.5C7.5 4.01472 9.51472 2 12 2C14.4853 2 16.5 4.01472 16.5 6.5Z"
                                             ></path>
                                         </svg>
-                                    </button>
-                                    <hr />
-                                    <span>14</span>
-                                </div>
-                                <div className="comment-container">
-                                    <div className="user">
-                                        <div className="user-pic">
-                                            <svg fill="none" viewBox="0 0 24 24" height="20" width="20" xmlns="http://www.w3.org/2000/svg">
-                                                <path
-                                                    strokeLinejoin="round"
-                                                    fill="#707277"
-                                                    strokeLinecap="round"
-                                                    strokeWidth="2"
-                                                    stroke="#707277"
-                                                    d="M6.57757 15.4816C5.1628 16.324 1.45336 18.0441 3.71266 20.1966C4.81631 21.248 6.04549 22 7.59087 22H16.4091C17.9545 22 19.1837 21.248 20.2873 20.1966C22.5466 18.0441 18.8372 16.324 17.4224 15.4816C14.1048 13.5061 9.89519 13.5061 6.57757 15.4816Z"
-                                                ></path>
-                                                <path
-                                                    strokeWidth="2"
-                                                    fill="#707277"
-                                                    stroke="#707277"
-                                                    d="M16.5 6.5C16.5 8.98528 14.4853 11 12 11C9.51472 11 7.5 8.98528 7.5 6.5C7.5 4.01472 9.51472 2 12 2C14.4853 2 16.5 4.01472 16.5 6.5Z"
-                                                ></path>
-                                            </svg>
-                                        </div>
-                                        <div className="user-info">
-                                            <span>{comment.comment_user_name}</span>
-                                            <p>{comment.comment_created_at.split("T")[0]}</p>
-                                        </div>
                                     </div>
-                                    <p className="comment-content">{comment.comment_body}</p>
-                                    <p>upvotes: {comment.comment_upvote} downvotes: {comment.comment_downvote}</p>
+                                    <div className="user-info">
+                                        <span>{comment.comment_user_name}</span>
+                                        <p>{comment.comment_created_at.split("T")[0]}</p>
+                                    </div>
                                 </div>
+                                <p className="comment-content">{comment.comment_body}</p>
                             </div>
                         </div>
+                    </div>
 
                 ))}
             </div>
