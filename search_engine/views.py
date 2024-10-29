f3fd0a8b5d228433ad9194d6543e49da99066f97
Alex Qin <pdsalex2018@gmail.com>
2024-10-29 15:37:03 -0400

    sorted based in rating for now

--- search_engine/views.py
+++ search_engine/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
 
 # Create your views here.
+
+
+
+def sort_data(request):
+    data = json.loads(request)
+    print(data)
+    return data
+
+
