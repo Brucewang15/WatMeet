7dfc354f5702c1ceac94b6ec9cc43c7bdb4e5473
Jonathan Gong <jonathangong2005@gmail.com>
2024-10-30 18:54:37 -0400

    Add user organization rating model and update comment fields
    - Introduced UserOrgRating model with relationships and validation.
    - Added created_at field to UserCommentRating and updated its default value.
    - Modified Comment model to set default values for title and body.

--- comments_and_ratings/models.py
+++ comments_and_ratings/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from datetime import datetime
+from django.utils import timezone
 
 class Comment(models.Model):
     comment_id = models.AutoField(primary_key=True)
-    comment_title = models.CharField(max_length=100, blank=True, null=True)
-    comment_body = models.TextField()
+    comment_title = models.CharField(max_length=100, blank=True, null=False, default="No Title")
+    comment_body = models.TextField(null=False, default="No Comment")
     user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='comments')
     org = models.ForeignKey('organizations.Organization', on_delete=models.CASCADE, related_name='comments')
     star_rating = models.IntegerField()
@@ -20,6 +23,23 @@ class UserCommentRating(models.Model):
     comment = models.ForeignKey('Comment', on_delete=models.CASCADE, related_name='user_comments_rating')
     upvote = models.BooleanField(default=False)
     downvote = models.BooleanField(default=False)
+    created_at = models.DateTimeField(
+        default=timezone.make_aware(datetime(2001, 9, 11, 8, 46))
+    )
 
     def __str__(self):
         return f"{self.user} - {self.comment}"
+ 
+class UserOrgRating(models.Model):
+    userorg_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='user_orgs_rating')
+    org = models.ForeignKey('organizations.Organization', on_delete=models.CASCADE, related_name='user_orgs_rating')
+    star_rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('user', 'org')
+
+    def __str__(self):
+        return f"{self.user} - {self.org}"
