12c7b82395438abe62e1b8e1fb795b8951977993
Alex Qin <pdsalex2018@gmail.com>
2024-11-01 19:22:39 -0400

    complete ver1 of search engine

--- organizations/sort/sort_data.py
+++ organizations/sort/sort_data.py
@@ -1,15 +1,19 @@
 import json
+from .title_match_score import get_match_score
 
 
 # this gives every organization a number corresponding what the user wishes to search
-def get_sort_num(org):
-    return org['star_rating']
+
 
 
 def sort_data(data, search_propt):
     data = list(data)
-    
-    data_with_num = list(map(lambda org: (get_sort_num(org), org), data))
+    print(data)
+    if (len(search_propt) >= 3):
+        data_with_num = list(map(lambda org: (get_match_score(org["org_name"], search_propt), org), data))
+    else:
+        data_with_num = list(map(lambda org: (org["star_rating"], org), data))
+
     data_with_num.sort(reverse=True, key=lambda s: s[0])
 
     sorted_data = list(map(lambda element: element[1], data_with_num))
---
+++ organizations/sort/title_match_score.py
@@ -0,0 +1,30 @@
+def get_match_score(club_name, query):
+    club_name = club_name.lower()
+    query = query.lower()
+    matches = 0
+
+    cn_trigram = {}
+    length = 0
+
+    # inserting trigrams to the dictionary
+    for i in range(len(club_name)):
+        if (len (club_name) - i <= 2):
+            break
+        trigram = club_name[i: i+3]
+        if (trigram in cn_trigram):
+            cn_trigram[trigram] += 1
+        else:
+            cn_trigram[trigram] = 1
+    #loop through query and see the matches
+    for i in range(len(query)):
+        if (len(query) - i <= 2):
+            break
+        trigram = query[i: i+3]
+        length += 1
+        if (trigram in cn_trigram):
+            matches += cn_trigram[trigram]
+    if (length == 0):
+        return 0
+    return matches/length # how many in the quiery matches the title of the club name
+
+
