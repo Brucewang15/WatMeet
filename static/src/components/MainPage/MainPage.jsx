fb7bbda293ee2c5677f4ffd19a027dffbf7a2bf7
Bruce Wang <bruce.wang15@outlook.com>
2024-11-02 16:45:36 -0400

    completed frontend for different org types

--- static/src/components/MainPage/MainPage.jsx
+++ static/src/components/MainPage/MainPage.jsx
@@ -12,7 +12,7 @@ import DisplayCard from '../UiComponents/DisplayCard';
 const MainPage = () => {
 
     const [clubs, setClubs] = useState([]);
-    const [searchPropt, setSearchPrompt] = useState(""); 
+    const [searchPropt, setSearchPrompt] = useState("");
 
     useEffect(() => {
         const get_club_data = async () => {
@@ -21,7 +21,7 @@ const MainPage = () => {
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({searchPropt: searchPropt})
+                body: JSON.stringify({ searchPropt: searchPropt })
             })
             if (response.ok) {
                 try {
@@ -35,23 +35,40 @@ const MainPage = () => {
         get_club_data()
     }, [searchPropt])
 
+
+    const [selectedType, setSelectedType] = useState('All');
+
+    const handleSelect = (type) => {
+        setSelectedType(type);
+    };
+
     return <>
-        <Header setSearchPropt={setSearchPrompt}/>
+        <Header setSearchPropt={setSearchPrompt} />
         <div className="mainContainer">
             <div className="chooseOrgType">
-                <div className="chooseOrgTypeButtons">Clubs</div>
-                <div className="chooseOrgTypeButtons">Design Teams</div>
-                <div className="chooseOrgTypeButtons">Sports</div>
-                <div className="chooseOrgTypeButtons">Intramurals</div>
+                {['All', 'Clubs', 'Design Teams', 'Sports', 'Intramurals'].map((type) => (
+                    <div
+                        key={type}
+                        className="chooseOrgTypeButtons"
+                        onClick={() => handleSelect(type)}
+                        style={{
+                            backgroundColor: selectedType === type ? 'white' : 'lightgray',
+                            color: selectedType === type? 'black' : '#666666',
+                            cursor: 'pointer'
+                        }}
+                    >
+                        {type}
+                    </div>
+                ))}
             </div>
             <div className="clubsContainer">
                 {clubs.map((club, index) => (
                     // <ClubsDisplay title={club.org_name} membershipType={club[1]} 
                     // description={club.overview} ranking_num = {club.ranking_num} 
                     // star_rating={club.star_rating} org_id = {club.org_id} key={index} />     
-                    <DisplayCard org_id = {club.org_id} clubName = {club.org_name} clubDescription={club.overview}
-                    clubRating = {club.star_rating} clubRatingNumber = {club.number_of_star_rating} clubRank = {club.ranking_num} />
-                    ))}
+                    <DisplayCard org_id={club.org_id} clubName={club.org_name} clubDescription={club.overview}
+                        clubRating={club.star_rating} clubRatingNumber={club.number_of_star_rating} clubRank={club.ranking_num} />
+                ))}
             </div>
         </div>
     </>
