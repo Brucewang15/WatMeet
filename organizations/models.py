e156ea8b569164205580f5aaf219245f4b3afe9d
Bruce Wang <bruce.wang15@outlook.com>
2024-11-07 15:37:38 -0500

    added organization socials into models

--- organizations/models.py
+++ organizations/models.py
@@ -18,6 +18,16 @@ class Organization(models.Model):
     org_type = models.CharField(max_length=20, choices=ORG_TYPE_CHOICES)
     constitution = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    instagram = models.TextField(blank=True, null=True)
+    facebook = models.TextField(blank=True, null=True)
+    linkedin = models.TextField(blank=True, null=True)
+    email = models.TextField(blank=True, null=True)
+    website = models.TextField(blank=True, null=True)
+    youtube = models.TextField(blank=True, null=True)
+    discord = models.TextField(blank=True, null=True)
+    linktr = models.TextField(blank=True, null=True)
+
+
 
     def __str__(self):
         return self.org_name
