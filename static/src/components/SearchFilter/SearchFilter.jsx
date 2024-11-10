f0a6bb1f1c8bcc4408717b0dde77d7b9a33c922f
Alex Qin <pdsalex2018@gmail.com>
2024-11-09 22:50:00 -0500

    added color change on click

--- static/src/components/SearchFilter/SearchFilter.jsx
+++ static/src/components/SearchFilter/SearchFilter.jsx
@@ -8,10 +8,32 @@ import "./SearchFilter.css";
 const SearchFilter = ({setRating}) => {
 
     const [slider, setSlider] = useState(0);
+    const [tags, setTags] = useState([
+        {id: 1, isClicked: false},
+        {id: 2, isClicked: false},
+        {id: 3, isClicked: false},
+        {id: 4, isClicked: false},
+        {id: 5, isClicked: false},
+        {id: 6, isClicked: false},
+        {id: 7, isClicked: false},
+        {id: 8, isClicked: false},
+        {id: 9, isClicked: false},
+        {id: 10, isClicked: false},
+        {id: 11, isClicked: false},
+        {id: 12, isClicked: false},
+    ]);
 
     const handleChange = (event) => {
         setSlider(event.target.value);
         setRating(event.target.value);
+    }
+
+    const handleTagClick = (id) => {
+        setTags(
+            tags.map((tag) => 
+                tag.id === id ? {id: tag.id, isClicked: !tag.isClicked} : tag
+            )
+        );
     }
 
 
@@ -30,10 +52,18 @@ const SearchFilter = ({setRating}) => {
             <div className="orgContainer">
                 <div className="orgType">Your organization type</div>
                 <div className="orgsContainer">
-                    <div className="orgs">Clubs</div>
-                    <div className="orgs">Design Teams</div>
-                    <div className="orgs">Sports</div>
-                    <div className="orgs">Intramurals</div>
+                    <div className={tags[0].isClicked ? "orgs clicked" : "orgs"} onClick={() => handleTagClick(1)}>Culture</div>
+                    <div className={tags[1].isClicked ? "orgs clicked" : "orgs"} onClick={() => handleTagClick(2)}>Recreation</div>
+                    <div className={tags[2].isClicked ? "orgs clicked" : "orgs"} onClick={() => handleTagClick(3)}>International</div>
+                    <div className={tags[3].isClicked ? "orgs clicked" : "orgs"} onClick={() => handleTagClick(4)}>Science</div>
+                    <div className={tags[4].isClicked ? "orgs clicked" : "orgs"} onClick={() => handleTagClick(5)}>Arts</div>
+                    <div className={tags[5].isClicked ? "orgs clicked" : "orgs"} onClick={() => handleTagClick(6)}>Community Service</div>
+                    <div className={tags[6].isClicked ? "orgs clicked" : "orgs"} onClick={() => handleTagClick(7)}>Academic</div>
+                    <div className={tags[7].isClicked ? "orgs clicked" : "orgs"} onClick={() => handleTagClick(8)}>Gaming</div>
+                    <div className={tags[8].isClicked ? "orgs clicked" : "orgs"} onClick={() => handleTagClick(9)}>Religious</div>
+                    <div className={tags[9].isClicked ? "orgs clicked" : "orgs"} onClick={() => handleTagClick(10)}>Performing Arts</div>
+                    <div className={tags[10].isClicked ? "orgs clicked" : "orgs"} onClick={() => handleTagClick(11)}>Technology</div>
+                    <div className={tags[11].isClicked ? "orgs clicked" : "orgs"} onClick={() => handleTagClick(12)}>Music</div>
                 </div>
             </div>
             <button className="filterClear">Clear Filter</button>
