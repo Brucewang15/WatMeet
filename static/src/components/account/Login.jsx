69b832716552939d6c4bcdcd670e6926def22d07
Bruce Wang <bruce.wang15@outlook.com>
2024-11-11 18:21:07 -0500

    created warning signs

--- static/src/components/account/Login.jsx
+++ static/src/components/account/Login.jsx
@@ -1,67 +1,80 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import "./General.css";
 import ConfirmEmail from './ConfirmEmail';
-import login from '../../redux/actions/authActions'
+import login from '../../redux/actions/authActions';
+import Warnings from '../warnings/Warnings';
 
 const Login = () => {
-
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const [isEmailSent, setIsEmailSent] = useState(false)
-
-    const forms = document.querySelectorAll('form');
-    forms.forEach(form => {
-        form.addEventListener('submit', function (event) {
-            event.preventDefault();
-        });
-    });
+    const [displayWarning, setDisplayWarning] = useState([false, ""]);
 
     const dispatch = useDispatch();
     const auth = useSelector(state => state.auth);
     const { emailVerificationRequired, isAuthenticated, error } = auth;
+
     const handleLogin = () => {
-        dispatch(login(email, password))
-    }
-    if (isAuthenticated) {
-        console.log('is authed', isAuthenticated)
-        window.location.href='/'
-    }
-    else if (emailVerificationRequired) {
-        console.log('sent email')
-    }
-    else {
-        console.log(error)
-    }
+        dispatch(login(email, password));
+    };
+
+    // Use useEffect to handle side effects and prevent infinite render
+    useEffect(() => {
+        if (isAuthenticated) {
+            console.log('is authed', isAuthenticated);
+            setDisplayWarning([false, ""])
+            window.location.href = '/';
+        } else if (emailVerificationRequired) {
+            console.log('sent email');
+        } else if (error) {
+            setDisplayWarning([true, error]);
+        }
+    }, [isAuthenticated, emailVerificationRequired, error]);
+
     return (
-        <div className='main-Container'>
-            {!emailVerificationRequired ? (
-                <div className="form-container">
-                    <p className="generalTitle">Login</p>
-                    <div className="form">
-                        <div className="input-group">
-                            <label for="email">Email</label>
-                            <input type="text" name="email" id="email" onChange={(e) => { setEmail(e.target.value) }} placeholder=""></input>
-                        </div>
-                        <div class="input-group">
-                            <label for="password">Password</label>
-                            <input type="password" name="password" id="password" onChange={(e) => { setPassword(e.target.value) }} placeholder=""></input>
-                            <div class="forgot">
-                                <a rel="noopener noreferrer" href="/forgot-password">Forgot Password ?</a>
+        <>
+            {displayWarning[0] && <Warnings warningType="Error" warningMessage={displayWarning[1]} />}
+            <div className="main-Container">
+                {!emailVerificationRequired ? (
+                    <div className="form-container">
+                        <p className="generalTitle">Login</p>
+                        <div className="form">
+                            <div className="input-group">
+                                <label htmlFor="email">Email</label>
+                                <input
+                                    type="text"
+                                    name="email"
+                                    id="email"
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    placeholder=""
+                                />
                             </div>
+                            <div className="input-group">
+                                <label htmlFor="password">Password</label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    placeholder=""
+                                />
+                                <div className="forgot">
+                                    <a rel="noopener noreferrer" href="/forgot-password">Forgot Password?</a>
+                                </div>
+                            </div>
+                            <button className="sign" onClick={handleLogin}>Sign in</button>
                         </div>
-                        <button class="sign" onClick={handleLogin}>Sign in</button>
+                        <p className="signup">
+                            Don't have an account? <a rel="noopener noreferrer" href="/signup">Sign up</a>
+                        </p>
                     </div>
-
-                    <p class="signup">Don't have an account?
-                        <a rel="noopener noreferrer" href="/signup" class="">Sign up</a>
-                    </p>
-                </div>
-            ) : (<ConfirmEmail email={email} password={password} />)}
-        </div>
+                ) : (
+                    <ConfirmEmail email={email} password={password} />
+                )}
+            </div>
+        </>
     );
-
-}
+};
 
 export default Login;
