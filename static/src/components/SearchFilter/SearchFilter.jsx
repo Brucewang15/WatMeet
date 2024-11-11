56184e3bb94579c78b54872e04d1b7cb911507a1
Bruce Wang <bruce.wang15@outlook.com>
2024-11-11 16:16:22 -0500

    updated club_info_tags

--- static/src/components/SearchFilter/SearchFilter.jsx
+++ static/src/components/SearchFilter/SearchFilter.jsx
@@ -1,15 +1,19 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
 import "./SearchFilter.css";
 
-const SearchFilter = ({setMinStarRating, setMinAmountRating}) => {
+const SearchFilter = ({ setMinStarRating, setMinAmountRating }) => {
 
     const [starRating, setStarRating] = useState(0);
     const [amountRating, setAmountRating] = useState(0);
-    const [tags, setTags] = useState(["Culture", "Recreation", "International", "Technology","Academic", "Arts", "Community Service", "Gaming", 
-        "Health", "Religious", "Performing Arts", "Music", "Science", "Charity", "Volunteering", "Fundraising", "Support Group", "Language", "Wellness", "Video Games", "Board Games", 
-        "Literature", "Public Speaking", "Law", "Mentorship", "Innovation", "Leadership", "Finance", "Sports", "Debate", "Engineering", "Investment", 
-        "LGBTQ+", "Film", "Diversity", "Education", "Photography", "Comedy", "Entrepreneurship", "Social", "Professional Development", "Networking", "Advocacy"]);
+    const [tags, setTags] = useState([
+        "Culture", "Recreation", "International", "Technology", "Academic", "Arts", "Community Service", "Gaming",
+        "Health", "Religious", "Performing Arts", "Music", "Science", "Charity", "Volunteering", "Fundraising",
+        "Support Group", "Language", "Wellness", "Literature", "Public Speaking", "Law", "Mentorship", "Innovation",
+        "Leadership", "Finance", "Sports", "Debate", "Engineering", "Investment", "LGBTQ+", "Film", "Diversity",
+        "Education", "Photography", "Comedy", "Entrepreneurship", "Social", "Professional Development", "Networking",
+        "Advocacy"
+    ]);
     const [isClicked, setIsClicked] = useState(new Array(tags.length).fill(false));
     const [expanded, setExpanded] = useState(false);
 
@@ -24,7 +28,7 @@ const SearchFilter = ({setMinStarRating, setMinAmountRating}) => {
     }
 
     const handleTagClick = (index) => {
-        setIsClicked(isClicked.map((item, i) => 
+        setIsClicked(isClicked.map((item, i) =>
             index == i ? !item : item
         ));
     }
@@ -33,7 +37,7 @@ const SearchFilter = ({setMinStarRating, setMinAmountRating}) => {
         if (expanded)
             return tags;
         let arr = [];
-        for (let i=0; i<10; i++) 
+        for (let i = 0; i < 10; i++)
             arr.push(tags[i]);
         return arr;
     }
@@ -53,14 +57,14 @@ const SearchFilter = ({setMinStarRating, setMinAmountRating}) => {
                     <div className="ratingText">Showing all ratings greater than:</div>
                     <div className="currentRating">{starRating}</div>
                 </div>
-                <input className="ratingInput" type="range" min="0" max="5" defaultValue="0" step="0.1" onChange={handleStarRatingChange}/>
+                <input className="ratingInput" type="range" min="0" max="5" defaultValue="0" step="0.1" onChange={handleStarRatingChange} />
             </div>
             <div className="ratingContainer">
                 <div className="showRating">
                     <div className="ratingText">Showing amount of ratings greater than:</div>
                     <div className="currentRating">{amountRating}</div>
                 </div>
-                <input className="ratingInput" type="range" min="0" max="5" defaultValue="0" step="0.1" onChange={handleAmountRatingChange}/>
+                <input className="ratingInput" type="range" min="0" max="5" defaultValue="0" step="0.1" onChange={handleAmountRatingChange} />
             </div>
             <div className="orgContainer">
                 <div className="orgType">Organization Tags</div>
